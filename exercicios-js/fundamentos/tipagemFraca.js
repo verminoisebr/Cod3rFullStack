8ea78d220d00a81710e7750a3f1b4d5e6970260e
Marcelo Boschi <marceloboschi78@hotmail.com>
2019-04-29 15:23:22 -0300

    Variables, types and behaviors

---
+++ exercicios-js/fundamentos/tipagemFraca.js
@@ -0,0 +1,12 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// EVITE NOME GENERICO E SIGLAS
+let valor = ''
+let numero = 1
+let pqp = false // PRODUTO QUIMICO PERGIOSO... KKK
