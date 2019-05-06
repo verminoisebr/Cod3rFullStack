103e6c10e2e675357d7f3b218fb4d76114d57789
Marcelo Boschi <marceloboschi78@hotmail.com>
2019-05-06 20:18:51 -0300

    Loops and Control Structures

---
+++ exercicios-js/controle/ifElse.js
@@ -0,0 +1,11 @@
+const imrpimirResultado = function(nota){
+    if (nota>=7){
+        console.log('Aprovado!')
+    } else{
+        console.log('Reprovado!')
+    }
+}
+
+imrpimirResultado(10)
+imrpimirResultado(4)
+imrpimirResultado('Epa!') //CUIDADO!!!
